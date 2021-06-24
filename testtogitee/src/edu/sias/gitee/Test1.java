3be2537710648e6f2ac4c81e03ebc2baa86e0d41
hqbcn <3098885773@qq.com>
2021-06-24 22:37:25 +0800

    测试IDEA项目到gitee

---
+++ testtogitee/src/edu/sias/gitee/Test1.java
@@ -0,0 +1,14 @@
+package edu.sias.gitee;
+/**
+ *@ClassName Test1
+ *@Description  TODO
+ *@Author HuangQingbin
+ *@Date 2021/6/24 22:34
+ *@Version 1.0
+ */
+public class Test1{
+    public static void main(String[] args) {
+        System.out.println("hello gitee");
+    }
+}
+
